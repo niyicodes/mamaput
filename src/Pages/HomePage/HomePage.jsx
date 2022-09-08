7f3c2b44a95581d827fdca758987dfdbaf499327
niyicodes <elijahtost@gmail.com>
2022-09-08 20:35:34 +0100

    half of homepage ready

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import CustomButton from '../../Components/Button/CustomButton';
+import './HomePage.scss'
 
 const HomePage = () => {
  return (
-  <div>Homepage</div>
+  <main>
+   <div className="landing-container">
+    <div className="textarea">
+     <h3>craving local meals?</h3>
+     <p>
+      Explore our menu of various home cooked meals delivered to your doorstep in an hour or less.
+     </p>
+     <CustomButton buttonName="Continue" />
+    </div>
+    <figure className="imagearea">
+     <img src="asset/Egus1i.png" alt="Egusi" />
+    </figure>
+   </div>
+   <img className="hot" src="asset/Hot.png" alt="Hot" />
+  </main>
  )
 }
 
