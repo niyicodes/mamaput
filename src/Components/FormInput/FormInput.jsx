3cc797575d6e2cddd7c64580b8aa72d543d1c35b
niyicodes <elijahtost@gmail.com>
2022-09-14 21:01:04 +0100

    contact us page ready

--- src/Components/FormInput/FormInput.jsx
+++ src/Components/FormInput/FormInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './FormInput.scss'
-const FormInput = ({handleChange, label, ...otherProps}) => {
+const FormInput = ({handleChange, label, placeholder, ...otherProps}) => {
  return (
   <div className='group'>
-   <input onChange={handleChange} className="form-input" {...otherProps}/>
+   <input onChange={handleChange} placeholder = {placeholder} className="form-input" {...otherProps}/>
    {
    label ? 
    (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Contact.scss'
+import FormInput from '../../Components/FormInput/FormInput'
+import CustomButton from '../../Components/Button/CustomButton'
 
 const Contact = () => {
  return (
-  <div>Contact</div>
+  <main>
+   <h3>contact us</h3>
+   <p>
+    Have any complaints? Or you want to reach out? Kindly fill the form below and be sure to hear back from us.
+   </p>
+   <form action="" className="form">
+    <FormInput placeholder='"Your name"'/>
+    <FormInput placeholder='"Your email"'/>
+    <FormInput placeholder='"Your phone"'/>
+    <textarea name="" id="" cols="30" rows="10" placeholder='"Message"'/>
+    <CustomButton buttonName="send message"/>
+   </form>
+  </main>
  )
 }
 
