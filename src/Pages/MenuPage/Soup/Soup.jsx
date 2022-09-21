57c05055ceb64153e74b9b03f9e0eec59cf27160
niyicodes <elijahtost@gmail.com>
2022-09-21 20:55:18 +0100

    data and setup for shop page almost ready

--- src/Pages/MenuPage/Soup/Soup.jsx
+++ src/Pages/MenuPage/Soup/Soup.jsx
@@ -2,49 +2,56 @@ import React, { useState } from "react";
 import HorizontalCard from "../../../Components/Card/HorizontalCard/HorizontalCard";
 import Navbar from "../../../Components/InnerNavbar/Navbar";
 import "./Soup.scss";
-
+import menu from '../../../Menu'
 const Soup = () => {
- const [currentItemPrice, setCurrentItemPrice] = useState(5000);
- const [addOnPrice, setAddOnPrice] = useState(0);
-
- const addOns = [
-  { id: 1, name: "eba", price: 500 },
-  { id: 2, name: "amala", price: 100 },
-  { id: 3, name: "fufu", price: 200 },
- ];
-
- console.log(currentItemPrice);
-
- return (
+  return (
   <div>
-   <div
-    style={{
-     borderRadius: "5px",
-     border: "1px solid #ccc",
-    }}
-   >
-     <Navbar />
-     <HorizontalCard />
-    {addOns &&
-     addOns.map((addon, i) => {
-      return (
-       <>
-        <input
-         type="checkbox"
-         name={addon.name}
-         value={addon.price}
-         onChange={(e) =>
-          setCurrentItemPrice(+currentItemPrice + +e.target.value)
-         }
-        />{" "}
-        <label>{addon.name}</label>
-        <br />
-       </>
-      );
-     })}
-   </div>
+   <Navbar />
+
+   {menu.map(({ id, image, name, description, rating, price, addOns }) =>{
+     return(
+       <HorizontalCard 
+        key={id}
+        image={image}
+        name={name}
+        description={description}
+        rating={rating}
+        price={price}
+        addOns={addOns}
+       />
+     )
+   })}
   </div>
  );
 };
 
 export default Soup;
+
+// {addOns &&
+//   addOns.map((addon, i) => {
+//    return (
+//     <>
+//      <input
+//       type="checkbox"
+//       name={addon.name}
+//       value={addon.price}
+//       onChange={(e) =>
+//        setCurrentItemPrice(+currentItemPrice + +e.target.value)
+//       }
+//      />{" "}
+//      <label>{addon.name}</label>
+//      <br />
+//     </>
+//    );
+//   })}
+
+// const [currentItemPrice, setCurrentItemPrice] = useState(5000);
+//  const [addOnPrice, setAddOnPrice] = useState(0);
+
+//  const addOns = [
+//   { id: 1, name: "eba", price: 500 },
+//   { id: 2, name: "amala", price: 100 },
+//   { id: 3, name: "fufu", price: 200 },
+//  ];
+
+//  console.log(currentItemPrice);
