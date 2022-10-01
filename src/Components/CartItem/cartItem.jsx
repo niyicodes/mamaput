6593a33574e098d2b98f56ae29c9fa0a150a6f05
niyicodes <elijahtost@gmail.com>
2022-10-01 22:09:05 +0100

    checkout item

--- src/Components/CartItem/cartItem.jsx
+++ src/Components/CartItem/cartItem.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import "./cartItem.scss";
 
-// const CartItem = ({ item: { id, image, price, name, quantity, addOns} }) => {
-//  return (
-//   <div className="cart-item" id={id}>
-//    <img src={image} alt={name} />
-//      <div className="item-details">
-
-//      </div>
-//   </div>
-//  );
-// };
-const CartItem = ({item: {id, image, price, name, quantity}}) => {
+const CartItem = ({ item: { id, image, price, name, quantity, addOns } }) => {
  return (
   <div className="cart-item" id={id}>
    <img src={image} alt={name} />
-   <div className="item-details">
-    <span className="name">{name}</span>
-    <span className="price">
-     {quantity} x {price}
-    </span>
-   </div>
+   {addOns ? (
+    addOns.map((addOn) => {
+     return (
+      <div className="item-details">
+       <span className="name">
+        {name} + {addOn.name}
+       </span>
+       <span className="price">
+        {quantity} x {price + addOn.price}
+       </span>
+      </div>
+     );
+    })
+   ) : (
+    <div className="item-details">
+     <span className="name">{name}</span>
+     <span className="price">
+      {quantity} x {price}
+     </span>
+    </div>
+   )}
   </div>
  );
 };
+// const CartItem = ({item: {id, image, price, name, quantity}}) => {
+//  return (
+//   <div className="cart-item" id={id}>
+//    <img src={image} alt={name} />
+//    <div className="item-details">
+//     <span className="name">{name}</span>
+//     <span className="price">
+//      {quantity} x {price}
+//     </span>
+//    </div>
+//   </div>
+//  );
+// };
 
 export default CartItem;
