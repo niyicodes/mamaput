e469d735974ec76f13dc27403161987e5bf53284
niyicodes <elijahtost@gmail.com>
2022-10-02 22:03:28 +0100

    some extra changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './Components/Header/Header';
 import HomePage from './Pages/HomePage/HomePage';
 import Contact from './Pages/Contact/Contact';
 import MenuPage from './Pages/MenuPage/MenuPage';
-// import Orders from './Pages/Orders/Orders';
 import SignInSignUp from './Pages/SignInSignUp/SignInSignUp';
 import Footer from './Components/Footer/Footer';
 import Soup from './Pages/MenuPage/Soup/Soup';
@@ -23,7 +22,6 @@ function App() {
       <Route path='/order' element={<CheckoutPage />}/>
       <Route path='/foodmenu' element={<MenuPage />}/>
       <Route path='/foodmenu/:name' element={<Soup />}/>
-      {/* <Route path='/orders' element={<Orders />}/> */}
       <Route path='/signIn-signUp' element={<SignInSignUp />}/>
      </Routes>
      <Footer />
